834247593a9098ed74480ae1021934e334d4af2b
Andreou Christos <ChristosAndre@hotmail.com>
2021-12-07 11:49:39 +0200

    dispaly data

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model) {
+
+        model.addAttribute("listEmployees", employeeService.getAllEmployees());
+        return "index";
+    }
+}
