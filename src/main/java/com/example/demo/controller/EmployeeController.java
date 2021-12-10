0b4d3886a0e229d3b34e5c5726b2fa1d868f471a
Andreou Christos <ChristosAndre@hotmail.com>
2021-12-10 13:35:57 +0200

    update service

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Employee;
 import com.example.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -17,5 +21,28 @@ public class EmployeeController {
 
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm (Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable (value = "id") long id, Model model) {
+
+        Employee employee = employeeService.getEmployeeById(id);
+
+        model.addAttribute("employee", employee);
+        return "update_employee";
     }
 }
--- src/main/java/com/example/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -18,4 +19,23 @@ public class EmployeeServiceImpl implements EmployeeService {
     public  List< Employee > getAllEmployees() {
         return employeeRepository.findAll();
     }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if (optional.isPresent()) {
+            employee = optional.get();
+        }else {
+            throw new RuntimeException("Employee not found for id :: " + id);
+        }
+        return employee;
+    }
+
+
 }
